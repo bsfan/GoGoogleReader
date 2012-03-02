636b28962fd3e5cd990005c92186dc2610a7d4cd
Tuo <clarkhtse@gmail.com>
2012-03-02 22:40:10 +0800

    Initial Commit

---
+++ GoGoogleReader/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  GoGoogleReader
+//
+//  Created by Tuo Huang on 3/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ GoGoogleReader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GoGoogleReader
+//
+//  Created by Tuo Huang on 3/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
